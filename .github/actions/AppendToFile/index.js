9ad3ea72ceb9f0d9ab962ce6ac5b30ff6facf300
kkeirstead <85592574+kkeirstead@users.noreply.github.com>
2022-09-29 12:40:41 -0700

    Update index.js

--- .github/actions/AppendToFile/index.js
+++ .github/actions/AppendToFile/index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const fs = require('fs');
+const path = require('path')
 
 const main = async () => {
     try {
@@ -10,28 +10,30 @@ const main = async () => {
 
         const insertFileNameParameter = "{insertFileName}";
 
-        if (paths !== null && paths.trim() !== "")
+        if (paths === null || paths.trim() === "")
         {
-            for (const path of paths.split(' ')) {
-                fs.readFile(path, (err, content) => {
-                    if (err) throw err;
-    
-                    if (!content.includes(textToSearch))
+            return;
+        }
+
+        for (const currPath of paths.split(' ')) {
+            fs.readFile(currPath, (err, content) => {
+                if (err) throw err;
+
+                if (!content.includes(textToSearch))
+                {
+                    var updatedTextToAdd = textToAdd;
+                    if (textToAdd.includes(insertFileNameParameter))
                     {
-                        var updatedTextToAdd = textToAdd;
-                        if (textToAdd.includes(insertFileNameParameter))
-                        {
-                            var encodedURI = encodeURIComponent(path);
-                            var encodedURIWithoutExtension = encodedURI.substring(0, encodedURI.length - 3) // remove the .md at the end
-                            updatedTextToAdd = textToAdd.replace(insertFileNameParameter, encodedURIWithoutExtension);
-                        }
-
-                        var contentStr = updatedTextToAdd + "\n\n" + content.toString();
-
-                        fs.writeFile(path, contentStr, (err) => {});
+                        const parsedPath = path.parse(currPath);
+                        const encodedURIWithoutExtension = encodeURIComponent(path.join(parsedPath.dir, parsedPath.name))
+                        updatedTextToAdd = textToAdd.replace(insertFileNameParameter, encodedURIWithoutExtension);
                     }
-                });
-            }
+
+                    var contentStr = updatedTextToAdd + "\n\n" + content.toString();
+
+                    fs.writeFile(currPath, contentStr, (err) => {});
+                }
+            });
         }
     } catch (error) {
         core.setFailed(error.message);
