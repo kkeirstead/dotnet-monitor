e57d3206d0d7af3189bd1262fb39ad4b2adecaa6
kkeirstead <kkeirstead@microsoft.com>
2022-09-29 13:48:39 -0700

    Tweaks

--- .github/actions/AppendToFile/index.js
+++ .github/actions/AppendToFile/index.js
@@ -2,7 +2,7 @@ const util = require("util");
 const fs = require('fs');
 const path = require('path')
 
-const main = async () => {
+async function main() {
 
     const jsExec = util.promisify(require("child_process").exec);
 
