a02e3aa6e76955585d5c058843e43beb4ea7ce4a
kkeirstead <85592574+kkeirstead@users.noreply.github.com>
2023-07-07 14:43:30 -0700

    Update TestAssemblies.cs

--- src/Tools/dotnet-monitor/TestAssemblies.cs
+++ src/Tools/dotnet-monitor/TestAssemblies.cs
@@ -1,6 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+// Look at this change!
+
 using Microsoft.AspNetCore.Hosting;
 using System;
 using System.Diagnostics;
