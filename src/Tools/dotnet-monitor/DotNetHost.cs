c37d2e680c45054dd70ef19e8944fdc353d0c3a6
kkeirstead <85592574+kkeirstead@users.noreply.github.com>
2023-10-02 14:36:54 -0700

    Update DotNetHost.cs

--- src/Tools/dotnet-monitor/DotNetHost.cs
+++ src/Tools/dotnet-monitor/DotNetHost.cs
@@ -1,4 +1,3 @@
-// Test 80
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
@@ -67,5 +66,3 @@ namespace Microsoft.Diagnostics.Tools.Monitor
         }
     }
 }
-
-// Testing 6
