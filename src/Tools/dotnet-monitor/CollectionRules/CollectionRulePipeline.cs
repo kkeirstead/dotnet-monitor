0187c7a81df27e712cd23f89ff6a241c1ffde858
kkeirstead <85592574+kkeirstead@users.noreply.github.com>
2023-10-02 12:58:01 -0700

    Update CollectionRulePipeline.cs

--- src/Tools/dotnet-monitor/CollectionRules/CollectionRulePipeline.cs
+++ src/Tools/dotnet-monitor/CollectionRules/CollectionRulePipeline.cs
@@ -1,7 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-// Testing a line shift 5.
+// Testing a line shift 6.
 
 using Microsoft.Diagnostics.Monitoring;
 using Microsoft.Diagnostics.Monitoring.WebApi;
