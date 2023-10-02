45a5b12f81354b9b37f41ac9512bead7c13e92c2
kkeirstead <85592574+kkeirstead@users.noreply.github.com>
2023-10-02 10:53:49 -0700

    Update CollectionRuleTriggerOptions.cs

--- src/Tools/dotnet-monitor/CollectionRules/Options/CollectionRuleTriggerOptions.cs
+++ src/Tools/dotnet-monitor/CollectionRules/Options/CollectionRuleTriggerOptions.cs
@@ -1,6 +1,7 @@
-﻿// kfjndslfa;sdkfj;sdafj // Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+// New line test.
 
 using Microsoft.Diagnostics.Monitoring.WebApi;
 using System.ComponentModel.DataAnnotations;
