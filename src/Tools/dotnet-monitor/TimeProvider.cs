5e0daaccbf69ca93654c56e90bfb3bfc88887866
kkeirstead <85592574+kkeirstead@users.noreply.github.com>
2023-10-06 13:40:40 -0700

    Update TimeProvider.cs

--- src/Tools/dotnet-monitor/TimeProvider.cs
+++ src/Tools/dotnet-monitor/TimeProvider.cs
@@ -1,4 +1,5 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 #if !NET8_0_OR_GREATER
